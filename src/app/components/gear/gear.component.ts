86aff8fb57f453ad25523dd687b3b4680bd1a8ba
Arthur <arthur@getblock.io>
2023-06-25 13:03:03 +0300

    init

---
+++ src/app/components/gear/gear.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gear',
+  templateUrl: './gear.component.html',
+  styleUrls: ['./gear.component.scss']
+})
+export class GearComponent {
+
+}
