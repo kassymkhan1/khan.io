8ecdf056025cf1d83053e0b77200a8b076c949b6
kassymkhan1 <kassymkhan24@gmail.com>
2023-06-25 13:03:03 +0300

    init

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { GearComponent } from './components/gear/gear.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    GearComponent,
+    ToolbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/components/blog/blog.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BlogComponent } from './blog.component';
+
+
+
+@NgModule({
+  declarations: [
+    BlogComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class BlogModule { }
