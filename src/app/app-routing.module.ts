6a469d4443ac7e469a98cebce82724f5363c91c0
kassymkhan1 <kassymkhan24@gmail.com>
2023-06-29 20:52:56 +0300

    add adaptive css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
-import { StartComponent } from './start/start.component';
 import { BlogComponent } from './components/blog/blog.component';
+import { AboutComponent } from './components/about/about.component';
+import { GearComponent } from './components/gear/gear.component';
 const routes: Routes = [
   { path: "home", component: HomeComponent},
-  { path: "about", component: StartComponent},
-  { path: "gear", component:  StartComponent},
+  { path: "about", component: AboutComponent},
+  { path: "gear", component:  GearComponent},
   { path: "blog", component: BlogComponent}
 ];
 
