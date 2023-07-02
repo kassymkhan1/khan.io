75b58314d4fb076fe835bc7fe3328ca32f57023d
kassymkhan1 <kassymkhan24@gmail.com>
2023-07-02 19:15:18 +0300

    update css

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -5,13 +5,13 @@ import { Github } from './github';
   providedIn: 'root'
 })
 export class ApiService {
-
   constructor(
-    private http: HttpClient
+    private http: HttpClient,
   ) { }
   github() {
     return this.http.get<Github[]>(`https://api.github.com/users/kassymkhan1/repos?per_page=100`)
   }
   youtube() { }
   blogs() { }
+  
 }
