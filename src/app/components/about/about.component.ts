816ea244d331b1cde857076ee24e88ae259756ce
kassymkhan1 <kassymkhan24@gmail.com>
2023-07-30 17:53:21 +0300

    update page about

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -6,15 +6,31 @@ import { ApiService } from 'src/app/services/api.service';
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent {
-  constructor() { }
-  ngOnInit(): void { }
-   
-  imgCollection: Array<object> = [
-      // {
-      //   image: 'https://loremflickr.com/g/600/400/paris',
-      //   thumbImage: 'https://loremflickr.com/g/1200/800/paris',
-      //   alt: 'Image 1',
-      //   title: 'Image 1'
-      // } 
-  ];
+  abouts = [
+    {
+      title: "About",
+      description: `
+        <p>
+        Hello world, I'm Kalzhan Kassymkhan!</p><br>
+        <p>
+        I'm a DevOps engineer currently working at international company. I have a passion for programming and am always looking for ways to incorporate it into my work.</p><br>
+        <p>
+        In addition to coding, I also make YouTube videos, where I focus on tech, creative vlogs, and personal development.<br>
+        </p>
+    `
+    },
+    {
+      title: "Connect",
+      description: `
+         Have a question or just want to chat? Feel free to email me. Try finding me anywhere else at @kassymmkhan24
+    `
+    },
+    {
+      title: "Work",
+      description: `
+      <p>3+ years of professional devops experience.</p><br>
+      <p>I started my career in university. Then I worked at a few small local companies.</p>
+    `
+    }
+  ]
 }
--- src/app/components/gear/gear.component.ts
+++ src/app/components/gear/gear.component.ts
@@ -1,20 +1,12 @@
 import { Component } from '@angular/core';
-
+import { GearService } from 'src/app/services/gear.service';
 @Component({
   selector: 'app-gear',
   templateUrl: './gear.component.html',
   styleUrls: ['./gear.component.scss']
 })
 export class GearComponent {
-  devices = [{
-    URL: "apple",
-    Name: "Macbook Pro 14",
-    Description: "Описание",
-    sponsored: false
-  }]
-  softwares = [{
-    URL: "apple",
-    Name: "Macbook Pro 14",
-    Description: "Описание"
-  }]
+  constructor(
+    public gear: GearService
+  ) { }
 }
