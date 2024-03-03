91d7a8ef2678aded93fde454e75f6dc986572b80
kassymkhan1 <kassymkhan24@gmail.com>
2024-03-03 19:10:12 +0300

    update gear page and css for home page

--- src/app/services/gear.service.ts
+++ src/app/services/gear.service.ts
@@ -11,7 +11,15 @@ export class GearService {
       {
         URL: 'apple',
         Name: 'Macbook Pro 14',
+        Image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSobj-8Pw04nd4JSrO4qT9_1ui2PWVLf1JnIQ&usqp=CAU",
         Description: 'Base machine for work',
+        sponsored: false,
+      },
+      {
+        URL: 'apple',
+        Name: 'Ipad Pro 11',
+        Image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxRLlzogCRG_bdmmtPDkfMemdXtU95gwccHw&usqp=CAU",
+        Description: 'My watch device',
         sponsored: false,
       },
     ];
@@ -21,10 +29,23 @@ export class GearService {
   Softs() {
     let softwares = [
       {
-        URL: 'apple',
-        Name: 'Macbook Pro 14',
-        Description: 'Описание',
+        URL: 'https://astronvim.com/',
+        Name: 'Nvim',
+        Image: "https://himynameisoleg.com/static/fc01ad89d221a3277846a09c20600fc9/fde66/astronvim.png",
+        Description: 'My best IDE',
       },
+      {
+        URL: 'https://linear.app',
+        Name: 'Linear',
+        Image: "https://linear.app/static/og/home.jpg",
+        Description: 'My productive app',
+      },
+      {
+        URL: 'https://slack.com',
+        Name: 'Slack',
+        Image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwZ0xsTd-O1JdW3HAvp6m8jHcSnp1QQ_Nw7Q&usqp=CAU",
+        Description: 'Communication Application',
+      }
     ];
 
     return softwares;
