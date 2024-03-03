71b36b43d1473aa9353ab1cc0196bf89215f41d6
kassymkhan1 <kassymkhan24@gmail.com>
2024-03-03 12:48:09 +0300

    update components

--- src/app/services/gear.service.ts
+++ src/app/services/gear.service.ts
@@ -1,29 +1,32 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class GearService {
-
   constructor() { }
 
   Techs() {
-    let devices = [{
-      URL: "apple",
-      Name: "Macbook Pro 14",
-      Description: "Base machine for work",
-      sponsored: false
-    }]
-    return devices
+    let devices = [
+      {
+        URL: 'apple',
+        Name: 'Macbook Pro 14',
+        Description: 'Base machine for work',
+        sponsored: false,
+      },
+    ];
+    return devices;
   }
 
   Softs() {
-    let softwares = [{
-      URL: "apple",
-      Name: "Macbook Pro 14",
-      Description: "Описание"
-    }]
+    let softwares = [
+      {
+        URL: 'apple',
+        Name: 'Macbook Pro 14',
+        Description: 'Описание',
+      },
+    ];
 
-    return softwares
+    return softwares;
   }
 }
